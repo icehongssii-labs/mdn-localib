6902b20e8fb8fab884f7e89f05350179459c7d30
douner <douner@dounerui-MacBookPro.local>
2024-06-11 18:46:19 +0900

    Feat: urls

--- locallib/urls.py
+++ locallib/urls.py
@@ -14,9 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include 
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),   
 ]
+
+urlpatterns += [
+    path('catalog', include('catalog.urls')),
+]
+
+# / -> catalog로 리다이렉트 
+urlpatterns += [ path('', RedirectView.as_view(url='catalog/', permanent=True))]
+# serving static files during dev
+urlpatterns += static(settings.STATIC_URL,
+                      document_root=settings.STATIC_ROOT)
