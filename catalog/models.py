dec6590f0741e37e4640f46567c7e8a1d27f2107
icehongssii12345 <icehongssii@gmail.com>
2024-06-12 23:39:54 +0900

    Feat: catalog model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db.models import UniqueConstraint
+from django.db.models.functions import Lower
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        help_text = "Enter a book genre"
+    )
 
 # Create your models here.
+# book
+# book instance
+# genre
+# writer
+
+
